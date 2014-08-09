6a572fe1e83e1407274a7e4d66583c3640269570
Michał <michaldul@outlook.com>
2014-08-09 15:24:06 +0200

    temporary changes

--- rbm.py
+++ rbm.py
@@ -183,7 +183,7 @@ class RBM(object):
 
         :param lr: learning rate used to train the RBM
 
-        :param k: number of Gibbs steps to do in CD-k/PCD-k
+        :param k: number of Gibbs steps to do in CD-k
 
         Returns a proxy for the cost and the updates dictionary. The
         dictionary contains the update rules for weights and biases.
@@ -193,11 +193,8 @@ class RBM(object):
         # compute positive phase
         pre_sigmoid_ph, ph_mean, ph_sample = self.sample_h_given_v(self.input)
 
-        # for CD, we use the newly generate hidden sample
-        chain_start = ph_sample
-
         # perform actual negative phase
-        # in order to implement CD-k/PCD-k we need to scan over the
+        # in order to implement CD-k we need to scan over the
         # function that implements one gibbs step k times.
         # Read Theano tutorial on scan for more information :
         # http://deeplearning.net/software/theano/library/scan.html
@@ -206,17 +203,17 @@ class RBM(object):
          pre_sigmoid_nhs, nh_means, nh_samples], updates = \
             theano.scan(self.gibbs_hvh,
                     # the None are place holders, saying that
-                    # chain_start is the initial state corresponding to the
+                    # ph_sample is the initial state corresponding to the
                     # 6th output
-                    outputs_info=[None,  None,  None, None, None, chain_start],
+                    outputs_info=[None,  None,  None, None, None, ph_sample],
                     n_steps=k)
 
         # determine gradients on RBM parameters
         # not that we only need the sample at the end of the chain
         chain_end = nv_samples[-1]
 
-        cost = T.mean(self.free_energy(self.input)) - T.mean(
-            self.free_energy(chain_end))
+        cost = self.free_energy(self.input) - self.free_energy(chain_end)
+
         # We must not compute the gradient through the gibbs sampling
         gparams = T.grad(cost, self.params, consider_constant=[chain_end])
 
@@ -226,77 +223,8 @@ class RBM(object):
             updates[param] = param - gparam * T.cast(lr,
                                                     dtype=theano.config.floatX)
         
-        # reconstruction cross-entropy is a better proxy for CD
-        monitoring_cost = self.get_reconstruction_cost(updates, pre_sigmoid_nvs[-1])
         
-        return monitoring_cost, updates
-
-    def get_pseudo_likelihood_cost(self, updates):
-        """Stochastic approximation to the pseudo-likelihood"""
-
-        # index of bit i in expression p(x_i | x_{\i})
-        bit_i_idx = theano.shared(value=0, name='bit_i_idx')
-
-        # binarize the input image by rounding to nearest integer
-        xi = T.round(self.input)
-
-        # calculate free energy for the given bit configuration
-        fe_xi = self.free_energy(xi)
-
-        # flip bit x_i of matrix xi and preserve all other bits x_{\i}
-        # Equivalent to xi[:,bit_i_idx] = 1-xi[:, bit_i_idx], but assigns
-        # the result to xi_flip, instead of working in place on xi.
-        xi_flip = T.set_subtensor(xi[:, bit_i_idx], 1 - xi[:, bit_i_idx])
-
-        # calculate free energy with bit flipped
-        fe_xi_flip = self.free_energy(xi_flip)
-
-        # equivalent to e^(-FE(x_i)) / (e^(-FE(x_i)) + e^(-FE(x_{\i})))
-        cost = T.mean(self.n_visible * T.log(T.nnet.sigmoid(fe_xi_flip -
-                                                            fe_xi)))
-
-        # increment bit_i_idx % number as part of updates
-        updates[bit_i_idx] = (bit_i_idx + 1) % self.n_visible
-
-        return cost
-
-    def get_reconstruction_cost(self, updates, pre_sigmoid_nv):
-        """Approximation to the reconstruction error
-
-        Note that this function requires the pre-sigmoid activation as
-        input.  To understand why this is so you need to understand a
-        bit about how Theano works. Whenever you compile a Theano
-        function, the computational graph that you pass as input gets
-        optimized for speed and stability.  This is done by changing
-        several parts of the subgraphs with others.  One such
-        optimization expresses terms of the form log(sigmoid(x)) in
-        terms of softplus.  We need this optimization for the
-        cross-entropy since sigmoid of numbers larger than 30. (or
-        even less then that) turn to 1. and numbers smaller than
-        -30. turn to 0 which in terms will force theano to compute
-        log(0) and therefore we will get either -inf or NaN as
-        cost. If the value is expressed in terms of softplus we do not
-        get this undesirable behaviour. This optimization usually
-        works fine, but here we have a special case. The sigmoid is
-        applied inside the scan op, while the log is
-        outside. Therefore Theano will only see log(scan(..)) instead
-        of log(sigmoid(..)) and will not apply the wanted
-        optimization. We can not go and replace the sigmoid in scan
-        with something else also, because this only needs to be done
-        on the last step. Therefore the easiest and more efficient way
-        is to get also the pre-sigmoid activation as an output of
-        scan, and apply both the log and sigmoid outside scan such
-        that Theano can catch and optimize the expression.
-
-        """
-
-        cross_entropy = T.mean(
-                T.sum(self.input * T.log(T.nnet.sigmoid(pre_sigmoid_nv)) +
-                (1 - self.input) * T.log(1 - T.nnet.sigmoid(pre_sigmoid_nv)),
-                      axis=1))
-
-        return cross_entropy
-
+        return updates
 
 def test_rbm(learning_rate=0.1, training_epochs=15,
              dataset='mnist.pkl.gz', batch_size=20,
@@ -325,8 +253,6 @@ def test_rbm(learning_rate=0.1, training_epochs=15,
     train_set_x, train_set_y = datasets[0]
     test_set_x, test_set_y = datasets[2]
 
-    print train_set_x.get_value(borrow=True)[0]
-
     # compute number of minibatches for training, validation and testing
     n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
 
@@ -341,8 +267,8 @@ def test_rbm(learning_rate=0.1, training_epochs=15,
     rbm = RBM(input=x, n_visible=28 * 28,
               n_hidden=n_hidden, numpy_rng=rng, theano_rng=theano_rng)
 
-    # get the cost and the gradient corresponding to one step of CD-15
-    cost, updates = rbm.get_cost_updates(lr=learning_rate, k=15)
+    # get the gradient corresponding to one step of CD-k
+    updates = rbm.get_cost_updates(lr=learning_rate, k=15)
 
     #################################
     #     Training the RBM          #
@@ -353,7 +279,7 @@ def test_rbm(learning_rate=0.1, training_epochs=15,
 
     # it is ok for a theano function to have no output
     # the purpose of train_rbm is solely to update the RBM parameters
-    train_rbm = theano.function([index], cost,
+    train_rbm = theano.function([index],
            updates=updates,
            givens={x: train_set_x[index * batch_size:
                                   (index + 1) * batch_size]},
@@ -366,11 +292,10 @@ def test_rbm(learning_rate=0.1, training_epochs=15,
     for epoch in xrange(training_epochs):
 
         # go through the training set
-        mean_cost = []
-        for batch_index in xrange(n_train_batches/100):
-            mean_cost += [train_rbm(batch_index)]
+        for batch_index in xrange(n_train_batches):
+            train_rbm(batch_index)
 
-        print 'Training epoch %d, cost is ' % epoch, numpy.mean(mean_cost)
+        print 'Training epoch %d' % epoch
 
         # Plot filters after each training epoch
         plotting_start = time.clock()
